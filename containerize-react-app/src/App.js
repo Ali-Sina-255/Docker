f0663e12c98738c9d8ed859bb60567621c2a9132
Ali Sina Sultani <alisinasultani255@gmail.com>
2025-09-17 00:06:33 +0430

    react project is containerize-react-app

--- containerize-react-app/src/App.js
+++ containerize-react-app/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h2>containerize react and django is is coming for docker</h2>
+      <h2>containerize react and django</h2>
     </div>
   );
 }
