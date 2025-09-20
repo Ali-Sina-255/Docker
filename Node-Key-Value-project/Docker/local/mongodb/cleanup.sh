d12f4bd8a0f44387882236eef88cd2e39bd6f54a
Ali Sina Sultani <alisinasultani255@gmail.com>
2025-09-20 06:36:14 +0430

    docker compose file

--- Node-Key-Value-project/Docker/local/mongodb/cleanup.sh
+++
@@ -1,7 +0,0 @@
-MONGODB_IMAGE="mongodb/mongodb-community-server"
-MONGODB_TAG="7.0-ubuntu2204"
-CONTAINER_NAME=" mongodb"
-
-# root credential
-
-docker run -d --rm --name $CONTAINER_NAME $MONGODB_IMAGE $MONGODB_TAG
---
+++ Node-Key-Value-project/db-config/mongo-init.js
@@ -0,0 +1,18 @@
+const keyValueDb = process.env.KEY_VALUE_DB;
+const keyValueUser = process.env.KEY_VALUE_USER;
+const keyValuePassword = process.env.KEY_VALUE_PASSWORD;
+console.log(
+  
+);
+
+db = db.getSiblingDB(keyValueDb);
+db.createUser({
+  user: keyValueUser,
+  pwd: KEY_VALUE_PASSWORD,
+  roles: [
+    {
+      role: "readWrite",
+      db: keyValueDb,
+    },
+  ],
+});
